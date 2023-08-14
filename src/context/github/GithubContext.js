9f3e0f30225b5d2f231b11c4bbd07700f62b39f2
Marcus Reis <marcus.reis@askblue.pt>
2023-08-14 22:05:37 +0100

    Get User Repos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
       users:[],
       user: {},
+      repos: [],
       loading: false
   }
 
@@ -43,7 +44,6 @@ export const GithubProvider = ({ children }) => {
     setLoading()
 
     const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-    //const response = await fetch(`${GITHUB_URL}/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`
       }
@@ -64,6 +64,30 @@ export const GithubProvider = ({ children }) => {
 
   }
 
+  // Get user repos
+  const getUserRepos = async (login) => {
+    setLoading()
+
+    const params = new URLSearchParams({
+      sort: 'stars',
+      per_page: 10
+    })
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    })
+
+    const data = await response.json()
+
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data,
+    })
+  }
+  
+
   // Clear users from state
   const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
 
@@ -77,9 +101,11 @@ export const GithubProvider = ({ children }) => {
       users: state.users,
       user: state.user,
       loading: state.loading,
+      repos: state.repos,
       searchUsers,
       getUser,
-      clearUsers
+      clearUsers,
+      getUserRepos
     }}
   >
       {children}
